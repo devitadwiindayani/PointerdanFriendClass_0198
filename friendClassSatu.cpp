a5dd8fd0acb739dc7ee677dcee4fa0f1a85c6765
devitadwiindayani <devitadwiindayani@gmail.com>
2025-05-05 15:57:50 +0700

    menambahkan metod pada friend class satu

--- friendClassSatu.cpp
+++ friendClassSatu.cpp
@@ -22,3 +22,15 @@ public:
     void setId(int pId);
     void displayAll(orang &a);
 };
+void orang::setNama(string pNama)
+{
+    nama = pNama;
+}
+void siswa::setId(int pId)
+{
+    id = pId;
+}
+void siswa::displayAll(orang &a)
+{
+    cout << id << endl << a.nama;
+}
